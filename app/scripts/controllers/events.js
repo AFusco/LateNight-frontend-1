c921a8b275d322947597627ddaf243c0383a82bb
Matteo Badini <mcode@Mac-Pro-di-Matteo.local>
2015-03-29 12:42:40 +0200

    aggiunta icone material svg + modifiche varie

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -10,9 +10,9 @@
 var app = angular.module('lateNightApp');
 
 app.controller('EventsCtrl', function ($scope) {
-    $scope.events = [
-        {idParty: 1, name: 'Estragon - Random Party', image:'./images.yeoman.png'},
-        {idParty: 2, name: 'Vintage - Closing Party'},
-        {idParty: 3, name: 'Matis - Fuori Corso'}
-    ];
+  $scope.events = [
+    {idParty: 1, name: 'Estragon - Random Party', image:'http://5.231.59.180/LateNight/party3.jpg', description: 'The titles of Washed Outs breakthrough song and the first single from Paracosm share the two most important words in Ernest Greenes musical language: feel it. Its a simple request, as well...'},
+    {idParty: 2, name: 'Vintage - Closing Party', image:'http://5.231.59.180/LateNight/party2.jpg', description: ''},
+    {idParty: 3, name: 'Numa - Extradate', image:'http://5.231.59.180/LateNight/party1.jpg', description: ''}
+  ];
 });
