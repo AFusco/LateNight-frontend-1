5398e54602575599036685590c536b087eaf3d69
Matteo Badini <mcode@Mac-Pro-di-Matteo.local>
2015-03-30 23:41:04 +0200

    material design siti web

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,21 @@
  * Controller of the lateNightApp
  */
 var app = angular.module('lateNightApp');
-app.controller('MainCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
-	$scope.toggleSidenav = function(menuId) {
-		$mdSidenav(menuId).toggle();
-	};
-}]);
+
+app.controller('MainCtrl', function($scope, $timeout, $mdSidenav, $log) {
+  $scope.toggleLeft = function() {
+    $mdSidenav('left').toggle()
+      .then(function(){
+        $log.debug('toggle left is done');
+      });
+  };
+});
+
+app.controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
+  $scope.close = function() {
+    $mdSidenav('left').close()
+      .then(function(){
+        $log.debug('close LEFT is done');
+      });
+  };
+});
